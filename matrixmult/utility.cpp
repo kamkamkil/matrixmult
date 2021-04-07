305d60fc327e2280ebaf1c559d9fd881573df173
kamil <kamil@user.com>
2021-04-07 20:29:03 +0200

    added init_matrix

--- matrixmult/utility.cpp
+++ matrixmult/utility.cpp
@@ -1,6 +1,35 @@
+#include <vector>
+
+/**
+ * @brief struktóra opisuj¹ca rozmiar macierzy 
+*/
 struct matrix_size {
-	size_t hight;
-	size_t width;
+	/**
+	 * @brief wysokoœæ macierzy
+	*/
+	const size_t hight;
+	/**
+	 * @brief szerokoœæ macierzy
+	*/
+	const size_t width;
 };
 
+/**
+ * @brief funkcjia inicjalizuje macierz danymi
+ * @tparam T typ danych w macierzy 
+ * @param size wielkoœæ macierzy 
+ * @param matrix macierz reprezentowana w wektorze 
+*/
+template <typename T>
+void init_matrix(matrix_size size, std::vector<T>& matrix)
+{
+	for (size_t i = 0; i < size.hight; i++)
+	{
+		for (size_t k = 0; k < size.width; k++)
+		{
+			// tymczasowe rozwi¹zanie potem chyba lepiej bêdzie to ³adowaæ z pliku
+			matrix[i * size.hight + k] = i * k;
+		}
+	}
+}
 
