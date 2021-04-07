dc98c84469139d84dfe676cd9d16cbeb85985b32
kamil <kamil@user.com>
2021-04-07 23:37:14 +0200

    added  get_coord

--- matrixmult/utility.cpp
+++ matrixmult/utility.cpp
@@ -28,8 +28,8 @@ void init_matrix(matrix_size size, std::vector<T>& matrix)
 	{
 		for (size_t k = 0; k < size.width; k++)
 		{
-			// tymczasowe rozwi¹zanie potem chyba lepiej bêdzie to ³adowaæ z pliku
-			matrix[i * size.width+ k] = i * 10 + k;
+			// tymczasowe rozwi¹zanie potem chyba lepiej bêdzie to ³adowaæ z pliku albo u¿ywaæ jakiœ losowych wartosci 
+			matrix[get_coord(size,i,k)] = i * 10 + k;
 		}
 	}
 }
@@ -41,7 +41,7 @@ void init_matrix(matrix_size size, std::vector<T>& matrix)
  * @param matrix macierz reprezentowana w wektorze
 */
 template <typename T>
-void print_matrix(matrix_size size, std::vector<T>& matrix)
+void print_matrix(matrix_size size, std::vector<T>& matrix) 
 {
 	for (size_t i = 0; i < size.hight; i++)
 	{
@@ -51,5 +51,17 @@ void print_matrix(matrix_size size, std::vector<T>& matrix)
 		}
 		std::cout << std::endl;
 	}
+}
+
+/**
+ * @brief zwraca wspó³¿edne macierzy do odczytania z wektora 
+ * @param size opis wielkoœci macierzy
+ * @param y który wiersz 
+ * @param x która kolumna 
+ * @return wpó³¿êdne 
+*/
+inline size_t get_coord(matrix_size size, size_t y, size_t x) 
+{
+	return y * size.width + x;
 }
 
